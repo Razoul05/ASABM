db1a6ffb656c3126474ded9b50d15ae2ae434ba6
lutzt <lutzt@RAZOULDT03>
2024-06-15 08:07:29 -0400

    Add project files.

---
+++ ASABM/Main.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace ASABM
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool rowAdding;
+        private int selectedRow;
+        private List<FileSystemWatcher> theWatchers;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ofd_FindArk = new System.Windows.Forms.OpenFileDialog();
+            this.d_BackupList = new System.Windows.Forms.DataGridView();
+            this.c_ARKName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_BackupCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_LastBackup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cm_ContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cm_Add = new System.Windows.Forms.ToolStripMenuItem();
+            this.cm_Remove = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.d_BackupList)).BeginInit();
+            this.cm_ContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ofd_FindArk
+            // 
+            this.ofd_FindArk.DefaultExt = "ark";
+            this.ofd_FindArk.Filter = "ARK files (*.ark)|*.ark";
+            // 
+            // d_BackupList
+            // 
+            this.d_BackupList.AllowUserToAddRows = false;
+            this.d_BackupList.AllowUserToDeleteRows = false;
+            this.d_BackupList.AllowUserToResizeRows = false;
+            this.d_BackupList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.d_BackupList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_ARKName,
+            this.c_BackupCount,
+            this.c_LastBackup});
+            this.d_BackupList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.d_BackupList.Location = new System.Drawing.Point(0, 0);
+            this.d_BackupList.Name = "d_BackupList";
+            this.d_BackupList.RowHeadersVisible = false;
+            this.d_BackupList.Size = new System.Drawing.Size(619, 187);
+            this.d_BackupList.TabIndex = 1;
+            this.d_BackupList.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.d_BackupList_CellValueChanged);
+            this.d_BackupList.SelectionChanged += new System.EventHandler(this.d_BackupList_SelectionChanged);
+            this.d_BackupList.Click += new System.EventHandler(this.d_BackupList_Click);
+            // 
+            // c_ARKName
+            // 
+            this.c_ARKName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.c_ARKName.HeaderText = "ARK Name";
+            this.c_ARKName.Name = "c_ARKName";
+            this.c_ARKName.ReadOnly = true;
+            this.c_ARKName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.c_ARKName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // c_BackupCount
+            // 
+            this.c_BackupCount.HeaderText = "Backup Count";
+            this.c_BackupCount.Name = "c_BackupCount";
+            // 
+            // c_LastBackup
+            // 
+            this.c_LastBackup.HeaderText = "Last Backup";
+            this.c_LastBackup.MinimumWidth = 150;
+            this.c_LastBackup.Name = "c_LastBackup";
+            this.c_LastBackup.ReadOnly = true;
+            this.c_LastBackup.Width = 150;
+            // 
+            // cm_ContextMenu
+            // 
+            this.cm_ContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cm_Add,
+            this.cm_Remove});
+            this.cm_ContextMenu.Name = "c_ContextMenu";
+            this.cm_ContextMenu.Size = new System.Drawing.Size(118, 48);
+            // 
+            // cm_Add
+            // 
+            this.cm_Add.Name = "cm_Add";
+            this.cm_Add.Size = new System.Drawing.Size(117, 22);
+            this.cm_Add.Text = "Add";
+            this.cm_Add.Click += new System.EventHandler(this.cm_Add_Click);
+            // 
+            // cm_Remove
+            // 
+            this.cm_Remove.Enabled = false;
+            this.cm_Remove.Name = "cm_Remove";
+            this.cm_Remove.Size = new System.Drawing.Size(117, 22);
+            this.cm_Remove.Text = "Remove";
+            this.cm_Remove.Click += new System.EventHandler(this.cm_Remove_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 187);
+            this.Controls.Add(this.d_BackupList);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(635, 226);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ASA Backup Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.d_BackupList)).EndInit();
+            this.cm_ContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog ofd_FindArk;
+        private System.Windows.Forms.DataGridView d_BackupList;
+        private System.Windows.Forms.ContextMenuStrip cm_ContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem cm_Add;
+        private System.Windows.Forms.ToolStripMenuItem cm_Remove;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_ARKName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_BackupCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_LastBackup;
+    }
+}
+
---
+++ ASABM/Main.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.IO.Compression;
+using System.Net.NetworkInformation;
+using System.Configuration;
+
+namespace ASABM
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            rowAdding = true; //flag row adding to prevent edit events from executing while setup is occuring
+            selectedRow = -1;
+            theWatchers = new List<FileSystemWatcher> ();
+
+            InitializeComponent();
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath); //Get app config from current exe path
+            for (int cnt = 0; cnt < config.AppSettings.Settings.Count; cnt++) //loop through each of the backup rules in the config file
+            {
+                String path = config.AppSettings.Settings["world" + cnt.ToString()].Value.Split(';')[0]; //each rule has a path;count, split them up
+                int count;
+                try
+                {
+                    count = Int32.Parse(config.AppSettings.Settings["world" + cnt.ToString()].Value.Split(';')[1]); //attempt to convert the count to an integer
+                }
+                catch
+                {
+                    count = 10; //default to 10 if int conversion fails
+                }
+
+                //Add new item to datagrid
+                int newrow = d_BackupList.Rows.Add();
+                d_BackupList["c_ARKName", newrow].Value = path;
+                d_BackupList["c_BackupCount", newrow].Value = count;
+                d_BackupList.ClearSelection();
+
+                //start file system watcher for this file
+                FileSystemWatcher watcher = new FileSystemWatcher(Path.GetDirectoryName(path));
+                watcher.Filter = Path.GetFileName(path);
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                watcher.Changed += WatcherFileChanged;
+                watcher.EnableRaisingEvents = true;
+
+                theWatchers.Add(watcher); //Add to watcher list to stop if removed from grid
+            }
+
+            rowAdding = false; //setup is complete, datagrid events can be processed now
+        }
+
+        private void d_BackupList_SelectionChanged(object sender, EventArgs e)
+        {
+            if (rowAdding) //a row is being added, either manually or object create so we want to ignore selection changes
+                return;
+
+            if (d_BackupList.SelectedCells.Count > 0 && d_BackupList.SelectedCells[0].ColumnIndex != d_BackupList.Columns["c_BackupCount"].Index) //we'll deselect the cell UNLESS its the backup count column selected
+                d_BackupList.ClearSelection();
+
+        }
+
+        private void d_BackupList_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (rowAdding || e.ColumnIndex != d_BackupList.Columns["c_BackupCount"].Index) //a row is being added, either manually or object create so we want to ignore value changes OR the edited row was NOT the backup count row
+                return;
+
+            d_BackupList.ClearSelection(); //deselect the current selectd cell
+            
+            int newCount;
+            try
+            {
+                newCount = Int32.Parse(d_BackupList[e.ColumnIndex, e.RowIndex].Value.ToString()); //read new count from datagrid and attempt to convert to INT
+            }
+            catch
+            {
+                newCount = 10; //int conversion failed, default to value of 10
+                d_BackupList[e.ColumnIndex, e.RowIndex].Value = 10;
+            }
+
+            //Update new backup count in config file
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            config.AppSettings.Settings.Remove("world" + e.RowIndex); //we need to remove this world from the config file
+            config.AppSettings.Settings.Add("world" + e.RowIndex, d_BackupList["c_ARKName", e.RowIndex].Value.ToString() + ";" + newCount.ToString()); //we'll re-add it to the config file with the new value
+            config.Save();
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+        }
+
+        private void d_BackupList_Click(object sender, EventArgs e)
+        {
+            //We handel the context menu displaying manually rather than binding it to the data grid so we can ensure
+            // that the remove option is enalbed/disabled as needed depending if a click occured in a row. without this
+            // the menu would display instantally from a right click and this method would be called
+
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == MouseButtons.Right) //If its a right click we want to show the context menu
+            {
+                if (d_BackupList.HitTest(me.X, me.Y).RowIndex >= 0)  //if the click occured on a row with data we can enable the remove menu item
+                {
+                    selectedRow = d_BackupList.HitTest(me.X, me.Y).RowIndex; //Save the row we clicked on in case we neeed it for a delete later
+                    cm_Remove.Enabled = true; //Enable the remove option
+                }
+                else
+                    cm_Remove.Enabled = false; //disable remove since a row was not clicked
+
+                cm_ContextMenu.Show(Cursor.Position); //show context menu at current mouse position.
+            }
+        }
+
+        private void cm_Add_Click(object sender, EventArgs e)
+        {
+            if (ofd_FindArk.ShowDialog() == DialogResult.OK) //Launch the open file dialog and if OK was pressed we can continue
+            {
+                for (int row = 0; row < d_BackupList.RowCount; row++) //loop through all files already in the datagrid to ensure this is not a duplicate
+                    if (d_BackupList["c_ARKName", row].Value.ToString() == ofd_FindArk.FileName)
+                    {
+                        //It was a duplicate, return error and cancel adding to application
+                        MessageBox.Show("ARK file already in backup list!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                        return;
+                    }
+
+                rowAdding = true; //We're adding a row so set this to prevent value changed events from being processed
+                int newrow = d_BackupList.Rows.Add();
+                d_BackupList["c_ARKName", newrow].Value = ofd_FindArk.FileName; //the full filename goes in the column ARKName
+                d_BackupList["c_BackupCount", newrow].Value = 10; //We'll default the backup count to 10
+                d_BackupList.ClearSelection(); //Clear the selected row
+                rowAdding = false; //we're done adding so events can be handeled again.
+
+                //Write this new world to the config file
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings.Add("world" + config.AppSettings.Settings.Count.ToString(), ofd_FindArk.FileName + ";10");
+                config.Save();
+                ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+
+                //Spawn file system watcher for this file
+                FileSystemWatcher watcher = new FileSystemWatcher(Path.GetDirectoryName(ofd_FindArk.FileName));
+                watcher.Filter = ofd_FindArk.SafeFileName;
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                watcher.Changed += WatcherFileChanged;
+                watcher.EnableRaisingEvents = true;
+
+                theWatchers.Add(watcher); //add new watcher to the list
+            }
+        }
+
+        private void cm_Remove_Click(object sender, EventArgs e)
+        {
+            theWatchers[selectedRow].Dispose(); //disable the filewatcher for this world
+            theWatchers.RemoveAt(selectedRow); //remove this filewatcher from list
+            d_BackupList.Rows.RemoveAt(selectedRow); //Remove this row from grid
+
+            //we need to rewrite the entire config file. The world are orginized as world0, wolrd1, etc.
+            // if world0 were to be removed that would leave only world1 and when the program is restarted
+            // and the config file is read it will see 1 total worlds but expect that count to start at 0.
+            //By rewritting the entire file we can ensure the worlds are always a contigious set starting at 0
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            int maxCount = config.AppSettings.Settings.Count; //Get the count of the CURRENT number of worlds in the config
+            for (int cnt = 0; cnt < maxCount; cnt++) //loop through each world
+                config.AppSettings.Settings.Remove("world" + cnt.ToString()); //remove this world from the config file
+
+            for (int cnt = 0;cnt < d_BackupList.RowCount; cnt++) //Loop through each world remaining in the grid
+                    config.AppSettings.Settings.Add("world" + cnt.ToString(), d_BackupList[0, cnt].Value.ToString() + ";" + d_BackupList[1, cnt].Value.ToString()); //readd this world to the config file but with the updated world#
+
+            config.Save(); //Save the config file
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+            selectedRow = -1; //reset selected row
+        }
+
+        private void WatcherFileChanged(object sender, FileSystemEventArgs e)
+        {
+            FileSystemWatcher watcher = (FileSystemWatcher)sender;
+            watcher.EnableRaisingEvents = false; //pause the raising of events for this watcher while we process this one. This prevents duplicate events from being processed
+
+            Thread.Sleep(10000); //Sleep for 10 seconds before acting, the file SHOULD be done being written to by now
+
+            String thisFile = e.FullPath;
+            for (int row = 0; row < d_BackupList.RowCount; row++) //Find this file in the datagrid so we can get the targeted number of backups
+                if (d_BackupList["c_ARKName", row].Value.ToString() == thisFile)
+                {
+                    String AntiCorruption = thisFile.Replace(Path.GetExtension(thisFile), @"_AntiCorruptionBackup.bak");
+                    if (File.Exists(AntiCorruption) && Math.Abs(((TimeSpan)(File.GetLastWriteTime(thisFile) - File.GetLastWriteTime(AntiCorruption))).TotalMinutes) <= 1)
+                        return; //If the target file and anticorruption have the same timestamp +- 1 minute we are on server startup and can skip this backup
+
+                    //We'll backup ALL arkprofiles and arktribes in the same folder as the world file even if they were not updated
+                    List<String> otherFiles = new List<String>();
+                    otherFiles.AddRange(Directory.GetFiles(Path.GetDirectoryName(thisFile), "*.arkprofile").ToList());
+                    otherFiles.AddRange(Directory.GetFiles(Path.GetDirectoryName(thisFile), "*.arktribe").ToList());
+
+                    ZipArchive zip = ZipFile.Open(Path.GetDirectoryName(thisFile) + @"\Backup_" + Path.GetFileNameWithoutExtension(thisFile) + "_" + DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss") + ".zip", ZipArchiveMode.Create);
+                    zip.CreateEntryFromFile(thisFile, Path.GetFileName(thisFile)); //Add world file to new zip file
+                    foreach (String file in otherFiles)
+                        zip.CreateEntryFromFile(file, Path.GetFileName(file)); //add other files to zip file
+                    zip.Dispose(); //close zip file
+
+                    List<String> otherBackups = Directory.GetFiles(Path.GetDirectoryName(thisFile), @"Backup_" + Path.GetFileNameWithoutExtension(thisFile) + "*.zip").ToList(); //Find all backups for this world
+                    otherBackups.Sort(); //sort the list, this should put the oldest at the top
+
+                    while (otherBackups.Count > Int32.Parse(d_BackupList["c_BackupCount", row].Value.ToString())) //loop as long as we have more than the max count
+                    {
+                        File.Delete(otherBackups[0]); //delete the top 1 file from the disk
+                        otherBackups.RemoveAt(0); //delete file record from list
+                    }
+
+                    d_BackupList.Invoke((MethodInvoker)delegate { d_BackupList["c_LastBackup", row].Value = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss"); }); //update datagrid with update time
+
+                    watcher.EnableRaisingEvents = true; //allow events to be raised again
+                    return;
+                }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Confirm IF we REALLY want to close
+            if (MessageBox.Show("Are you sure you want to close?\n\nBackup operations will stop!", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+                e.Cancel = true;
+        }
+    }
+}
